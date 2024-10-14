764b5e81fb5592a91ebed51ec5f2c8ef6471567f
matthiashaasesourceforge <Matthias.Haase@t-systems.com>
2024-10-14 17:41:39 +0200

    [skip ci] 20241014_174139 - update pod.sh

--- images/nginx/rootfs/pod.sh
+++ images/nginx/rootfs/pod.sh
@@ -2,10 +2,12 @@
 kubectl exec -it docker -- sh -c "\
 docker version;\
 apk update;\
-apk add -f curl git mc pax vim unzip zip;\
+apk add -f curl git mc vim unzip zip;\
 git clone https://github.com/matthias-haase/ingress-nginx.git;\
 cd /ingress-nginx;\
 git checkout patch-opentelemetry-cpp-and-contrib-and-proto;\
 git status;\
+cd /ingress-nginx/images/nginx/rootfs;\
+sh build.sh;\
 ls;\
 echo END;"
