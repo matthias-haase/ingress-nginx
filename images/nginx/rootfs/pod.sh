b8cfd46d254e6a14c743a5abbcfef13d9f6da01e
matthiashaasesourceforge <Matthias.Haase@t-systems.com>
2024-10-14 17:47:21 +0200

    [skip ci] 20241014_174721 - update pod.sh

--- images/nginx/rootfs/pod.sh
+++ images/nginx/rootfs/pod.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+kubectl delete -f docker-pod.yaml
+sleep 1
 kubectl apply -f docker-pod.yaml
 kubectl wait -n default --for=condition=Ready pod --all
+MAX=15
+for i in $(seq 1 $MAX)
+do
+echo "sleep $i/$MAX"
+sleep 1
+done
 kubectl exec -it docker -- sh -c "\
 docker version;\
 apk update;\
