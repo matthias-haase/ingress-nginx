5c15bb9cc1a42b5729b0ed4e1d02b5bbb803c5ee
matthiashaasesourceforge <Matthias.Haase@t-systems.com>
2024-10-15 14:38:21 +0200

    [skip ci] 20241015_143821 - update rootfs/build.sh

--- images/nginx/rootfs/build.sh
+++ images/nginx/rootfs/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-set -x
-#echo "140.82.121.4 github.com" >> /etc/hosts
-#tail /etc/hosts
-#curl -v github.com
-#sleep 10
+
 # Copyright 2023 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
